3b350d5a7a211d2ee243414be04787656d4ad6ec
TimyrAkimov <78687130+TimyrAkimov@users.noreply.github.com>
2023-01-30 23:02:29 +0300

    set localization and basic markup/styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="app-header">
+        <div className="w-full justify-between my-auto flex h-[60px]">
+        <img className="w-12 h-12 float-left ml-24 my-auto" src="/images/logo.svg" alt="logo" />
+        <a className="text-xl my-auto float-left mr-[57rem] text-grey hover:text-dark-green">WeatherApp</a>
+        <div className="float-right mr-24 space-x-6 text-grey my-auto underline">
+          <a className="text-dark-green" href="main.html">Main</a>
+          <a className="hover:text-dark-green" href="about.html">About</a>
+          <a className="hover:text-dark-green" href="feedback.html">Get in touch</a>
+        </div>
+        </div>
+        </div>
       </header>
+
+      <body>
+        <div className="content">
+
+        </div>
+      </body>
+
+      <footer>
+        <div className="footer">
+          <p className="ml-24 pt-5 text-base text-grey">&copy; 2022 WeatherApp. All Rigths Reserved.</p>
+        </div>
+      </footer>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+      "lavender": "#7E3AF2",
+      "white": "#FCFBFA",
+      "black": "#27272A",
+      "grey": "#E5E7EB",
+      "dark-green": "#09D3AC"
+      },
+    },
+  },
+  plugins: [],
+}
